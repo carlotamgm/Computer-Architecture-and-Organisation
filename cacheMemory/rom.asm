d702482c25ee1b96de396475aa3cda270b403e6f
carlotamgm <129082159+carlotamgm@users.noreply.github.com>
2024-10-18 18:33:02 +0200

    DESIGN OF AN INSTRUCTIONS CACHE MEMORY

---
+++ cacheMemory/rom.asm
@@ -0,0 +1,14 @@
+/*
+Una dirección que falle en el conjunto 0 (ciclos 0-12) 0000001 00000 0000
+Una dirección que falle en el conjunto 0 (ciclos 13-25) 0000010 00000 0000
+Una dirección que falle en el conjunto 0 (ciclos 26-38) 0000011 00000 0000
+Una dirección que acierte en el conjunto 0 via 1 (ciclo 39) 0000010 00000 0100
+Una dirección que falle en el conjunto 20 (ciclos 40-52) 0000101 10100 0000
+Una dirección que falle en el conjunto 20 (ciclos 53-65) 0000111 10100 0000
+Una dirección que falle en el conjunto 20 (ciclos 66-78) 0001001 10100 0000
+Una dirección que acierte en el conjunto 20 via 0 (ciclo 79) 0001001 10100 0100
+Una dirección que falle en el conjunto 0 (ciclos 80-92) 0010000 00000 0000
+Una dirección que falle en el conjunto 0 (ciclos 93-105) 0011000 00000 0000
+Una dirección que falle en el conjunto 0 (ciclos 106-118) 0100000 00000 0000
+Una dirección que acierte en el conjunto 0 via 0 (ciclo 119) 0011000 00000 0100
+*/
